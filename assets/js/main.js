55b32c4dbf89678b48a118616862763ddd45846f
Hemanath <hemanathpd11@gamil.com>
2025-08-13 15:41:59 +0530

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,104 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
+// Show menu
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  })
+}
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+// Hide menu
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  })
+}
 
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+function linkAction(){
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
+/*=============== ACCORDION SKILLS ===============*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
 
-/*==================== REMOVE MENU MOBILE ====================*/
+function toggleSkills(){
+  let itemClass = this.parentNode.className;
 
+  for(let i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close';
+  }
+  if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open';
+  }
+}
+skillsHeader.forEach(e => {
+  e.addEventListener('click', toggleSkills);
+});
 
-/*==================== ACCORDION SKILLS ====================*/
+/*=============== QUALIFICATION TABS ===============*/
+// If you use qualification tabs, implement here
 
+/*=============== SERVICES MODAL ===============*/
+// If you use services modal, implement here
 
-/*==================== QUALIFICATION TABS ====================*/
+/*=============== PORTFOLIO SWIPER ===============*/
+let swiperPortfolio = new Swiper('.portfolio__container', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
+/*=============== TESTIMONIAL SWIPER ===============*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints:{
+    568:{
+      slidesPerView: 2,
+    }
+  }
+});
 
-/*==================== SERVICES MODAL ====================*/
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+function scrollActive(){
+  const scrollY = window.pageYOffset;
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
 
-/*==================== PORTFOLIO SWIPER  ====================*/
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('nav-link-active');
+    }else{
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('nav-link-active');
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive);
 
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+  const header = document.getElementById
